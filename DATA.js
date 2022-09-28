e4319650e57b31c3d0a143442cc96b8822a18603
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 15:07:22 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "a44e4eb1-b4bd-44a1-940a-41f8e1284261",
+    "home_feed": [
+        {
+            "id": "90edbcec-b98c-4ae5-a817-017d70abec4b",
+            "artist_name": "world",
+            "home_carousel": [
+                {
+                    "id": "27cb3a25-be98-4c4e-a76d-0290ecd70c2a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                },
+                {
+                    "id": "89fce041-c3cb-4203-bf24-3c501ece6244",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png"
+                },
+                {
+                    "id": "6f57445b-5991-401c-8e9c-5c71ccef3c0e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                },
+                {
+                    "id": "72c38806-6e70-4cfa-800b-a9ba4c8c7c5f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/87.png"
+                },
+                {
+                    "id": "ce2cca54-fc9d-4bd1-9a3c-93b9c10fec47",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                }
+            ],
+            "art_caption": "money",
+            "art_description": "Across would yourself. Court about according attention half trip. Watch personal deal chair."
+        },
+        {
+            "id": "28882f48-828c-4302-ad75-2a85927e3c19",
+            "artist_name": "perhaps",
+            "home_carousel": [
+                {
+                    "id": "206153b5-87e0-4919-a342-50898019ea40",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "61069959-0a39-4297-96a6-b801e2a1b982",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/325.png"
+                },
+                {
+                    "id": "1ed1dff0-8109-4a2a-b31c-32969560ca64",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/112.png"
+                },
+                {
+                    "id": "4d974274-4e06-4443-a3eb-f6c1ab210d25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png"
+                },
+                {
+                    "id": "351f643f-a7dc-497b-9d80-ec6b094ba363",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                }
+            ],
+            "art_caption": "but",
+            "art_description": "Cell decide too about whole. Different mind return few security."
+        },
+        {
+            "id": "ee6bb32a-23a0-4457-bee7-7fa3faf1a824",
+            "artist_name": "fall",
+            "home_carousel": [
+                {
+                    "id": "90cd7dfb-39e6-46f4-a6a9-83509afac630",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png"
+                },
+                {
+                    "id": "194786e2-474d-47c8-b535-194496497c7b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                },
+                {
+                    "id": "1f32dd88-a34b-4385-b671-5700a04afdb9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                },
+                {
+                    "id": "b7becc7a-1960-48fb-9a53-5190ef6a319e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/328.png"
+                },
+                {
+                    "id": "72882b7f-9ad8-4603-a914-bec60dcb0560",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                }
+            ],
+            "art_caption": "another",
+            "art_description": "Several fish prove investment contain. Century growth capital major about."
+        },
+        {
+            "id": "896bc32b-9ec1-4600-80ed-04e533e3ba16",
+            "artist_name": "fight",
+            "home_carousel": [
+                {
+                    "id": "424f709b-f060-408f-9f0e-bd89955c070b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                },
+                {
+                    "id": "3d463fb9-f4c7-4593-a7a4-065442b88da7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                },
+                {
+                    "id": "72c46ecf-c92d-4cab-a2f9-c8e1cbd172d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                },
+                {
+                    "id": "30966b4c-7412-4cd8-8aa6-c6df305c1f74",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png"
+                },
+                {
+                    "id": "cccb4042-a15f-4374-b5f3-7316ea08d511",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/49.png"
+                }
+            ],
+            "art_caption": "debate",
+            "art_description": "Too writer rise pick may. Run well and in. Join my go tough bill truth subject."
+        },
+        {
+            "id": "15cc7cb0-aebf-4bfb-821d-685e465e4baa",
+            "artist_name": "pass",
+            "home_carousel": [
+                {
+                    "id": "a552c2fc-961d-4d59-8ef9-de31c06656d0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png"
+                },
+                {
+                    "id": "7f69b025-6461-4c7f-b016-93afcd7a6245",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                },
+                {
+                    "id": "7fa78bc8-dada-40c0-adf6-df69952d9faa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/224.png"
+                },
+                {
+                    "id": "98870f30-e566-4706-8e68-6b6318179b0a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                },
+                {
+                    "id": "b8eceba0-4fc9-4513-a269-1ef0f7e15be4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                }
+            ],
+            "art_caption": "leave",
+            "art_description": "American actually trip in remember get mission. Card skill often surface rather. Growth national success view open."
+        }
+    ]
+}
